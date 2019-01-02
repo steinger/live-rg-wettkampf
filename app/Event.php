972542c1c04de04f963a3eca44314682f224e79b
Steinger Marcel <github@steinger.ch>
2019-01-02 22:48:32 +0100

    DB::raw without NVL

--- app/Event.php
+++ app/Event.php
@@ -21,7 +21,7 @@ class Event extends Model
     {
         $data = DB::table('events as e')
                           ->select('e.id', 'e.name', 'e.created_at',
-                            DB::raw("(SELECT NVL(e2.file,'') FROM events e2
+                            DB::raw("(SELECT e2.file FROM events e2
                                   WHERE e2.id = e.id and e2.file is not null and e2.updated_at < NOW() - INTERVAL 1 HOUR) AS file"))
                           ->orderBy('e.id','desc')
                           ->get();
