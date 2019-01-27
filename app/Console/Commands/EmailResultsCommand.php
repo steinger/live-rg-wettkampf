0ca11370c76136920e8af01abb3895fa8dbef194
Marcel Steinger <github@steinger.ch>
2019-01-27 16:13:42 +0100

    Add new email parse on EmailResultsCommand

--- app/Console/Commands/EmailResultsCommand.php
+++ app/Console/Commands/EmailResultsCommand.php
@@ -80,6 +80,7 @@ class EmailResultsCommand extends Command
      */
     public function InputData($emailData, $body, $fromEmail)
     {
+        // Parse first part
         $header = explode(" ", $emailData,4);
         $body = explode("---", $body,2);
         $body = str_replace("\r\n"," ",$body[0]);
@@ -91,26 +92,45 @@ class EmailResultsCommand extends Command
             $data = [];
             $data['name'] = $body;
             $data['file'] = "";
+            $data['ranking'] = 0;
             $data['created_at'] = now();
             $data['updated_at'] = now();
             $event->insert($data);
         }
         else
         {
+            // Parse secound part
+            $headerNameArray = explode(" ", $header[3]);
+            $category = array_pop($headerNameArray);
+            $headerName = implode(" ", $headerNameArray);
+            $bodyArray = array_pad(explode(",", $body), 4, '');
+            $firstBodyArray = explode(" ", $bodyArray[0]);
+            $f_score = array_pop($firstBodyArray);
+            $apparatus = implode(" ", $firstBodyArray);
+            $d_scoreArray = explode(":", $bodyArray[1]);
+            $d_score = array_pop($d_scoreArray);
+            $e_scoreArray = explode(":", $bodyArray[2]);
+            $e_score = array_pop($e_scoreArray);
+            // Get last Event ID
             $event = new Event;
             $lastEventID = $event->max('id');
             // Log::debug($fromEmail.";Info;".$header[0]);
-            $result = new Result;
             // Count
+            $result = new Result;
             $rowCount = $result->all()->where('rgid',$header[0])->where('event_id',$lastEventID)->count();
             if ($rowCount > 0)
             {
                 // Update
                 $result_update = $result->where('rgid',$header[0])->where('event_id',$lastEventID)->first();
-                $result_update->apparatus = $header[1];
+                $result_update->apparatus_short = $header[1];
                 $result_update->startno = $header[2];
-                $result_update->name = $header[3];
-                $result_update->body = $body;
+                $result_update->name = $headerName;
+                $result_update->category = $category;
+                $result_update->apparatus = $apparatus;
+                $result_update->f_score = $f_score;
+                $result_update->d_score = trim($d_score);
+                $result_update->e_score = trim($e_score);
+                $result_update->penalty = trim($bodyArray[3]);
                 $result_update->save();
             }
             else
@@ -119,10 +139,16 @@ class EmailResultsCommand extends Command
                 $data = [];
                 $data['rgid'] = $header[0];
                 $data['event_id'] = $lastEventID;
-                $data['apparatus'] = $header[1];
+                $data['apparatus_short'] = $header[1];
                 $data['startno'] = $header[2];
-                $data['name'] =  $header[3];
-                $data['body'] =  $body;
+                $data['name'] = $headerName;
+                $data['category'] = $category;
+                $data['competition_type'] = 'MK';
+                $data['apparatus'] = $apparatus;
+                $data['f_score'] = $f_score;
+                $data['d_score'] = trim($d_score);
+                $data['e_score'] = trim($e_score);
+                $data['penalty'] = trim($bodyArray[3]);
                 $data['created_at'] = now();
                 $data['updated_at'] = now();
                 $result->insert($data);
