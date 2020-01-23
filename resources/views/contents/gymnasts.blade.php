1cd8d80b3c8434f51e79dfd2eadf692bd30f7c1c
Marcel Steinger <github@steinger.ch>
2020-01-23 21:07:12 +0100

    If no gymnast data then info message

--- resources/views/contents/gymnasts.blade.php
+++ resources/views/contents/gymnasts.blade.php
@@ -6,31 +6,39 @@
 
 @section('content')
 
-<div class="container mt-2">
-  <div class="alert alert-info">
-    @foreach( $names as $name )
-    <h4 class="alert-heading">{{$name->startno}} - {{$name->name}} &middot; {{$name->category}}</h4>
-    @endforeach
-  </div>
-  <ul class="list-group">
-    @foreach( $results as $result )
-    <li class="list-group-item">
-      <div class="media">
-          @if($result->apparatus_short)
-            <img class="mr-3" src="{{ asset('images/'.$result->apparatus_short.'.png') }}" alt="rg image">
-          @else
-            <img class="mr-3" src="{{ asset('images/apple-icon.png') }}" alt="rg image" width="80">
-          @endif
-        <div class="media-body">
-          <h4>{{ $result->apparatus }}: {{ $result->f_score }}</h4>
-          <strong>D:{{ $result->d_score }} E:{{ $result->e_score }} {{ $result->penalty}}</strong>
-          <br />
-          <i class="far fa-clock"></i> {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $result->updated_at)->isoFormat('lll')}} &middot; <small>{{ $result->updated_at_humans }}</small>
+  @if (count($names))
+  <div class="container mt-2">
+    <div class="alert alert-info">
+      @foreach( $names as $name )
+      <h4 class="alert-heading">{{$name->startno}} - {{$name->name}} &middot; {{$name->category}}</h4>
+      @endforeach
+    </div>
+    <ul class="list-group">
+      @foreach( $results as $result )
+      <li class="list-group-item">
+        <div class="media">
+            @if($result->apparatus_short)
+              <img class="mr-3" src="{{ asset('images/'.$result->apparatus_short.'.png') }}" alt="rg image">
+            @else
+              <img class="mr-3" src="{{ asset('images/apple-icon.png') }}" alt="rg image" width="80">
+            @endif
+          <div class="media-body">
+            <h4>{{ $result->apparatus }}: {{ $result->f_score }}</h4>
+            <strong>D:{{ $result->d_score }} E:{{ $result->e_score }} {{ $result->penalty}}</strong>
+            <br />
+            <i class="far fa-clock"></i> {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $result->updated_at)->isoFormat('lll')}} &middot; <small>{{ $result->updated_at_humans }}</small>
+          </div>
         </div>
-      </div>
-    </li>
-    @endforeach
-  </ul>
+      </li>
+      @endforeach
+    </ul>
+  </div>
+@else
+<div class="container">
+  <div class="list-group">
+    <button type="button" class="list-group-item list-group-item-info">{{__('No data available.')}}</button>
+  </div>
 </div>
+@endif
 
 @endsection
