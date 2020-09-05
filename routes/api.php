73092ad7a65965213a309e541d8a4c2adfd15b90
Marcel Steinger <github@steinger.ch>
2020-09-05 21:52:30 +0200

    Change api middleware route

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'UserController@AuthRouteAPI');
+
 Route::get('/', 'ContentController@homeApi');
 Route::get('/live/{page}', 'ContentController@liveApi');
 Route::get('/list/{event_id}', 'ContentController@listApi');
